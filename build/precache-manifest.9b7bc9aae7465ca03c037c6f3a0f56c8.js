e421e8f85fff1506f179b479ab99f178d52cb85e
dacha dallocort <dragan.majstorovic@gmail.com>
2020-11-16 14:08:31 +0100

    Initial commit

---
+++ build/precache-manifest.9b7bc9aae7465ca03c037c6f3a0f56c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589f224fa3b788b6b2b9171dfadc2584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "517101d80c832dd74542",
+    "url": "/static/css/main.402e6925.chunk.css"
+  },
+  {
+    "revision": "2777a80ab9cea94c97f1",
+    "url": "/static/js/2.7a0073ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a0073ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517101d80c832dd74542",
+    "url": "/static/js/main.6b06115c.chunk.js"
+  },
+  {
+    "revision": "2b0e5f78672a0df61f7f",
+    "url": "/static/js/runtime-main.b87ea116.js"
+  },
+  {
+    "revision": "21b45239b2987e98db424d8f349ba20b",
+    "url": "/static/media/clock.21b45239.svg"
+  }
+]);
